bdc78b2f10dff106d97bf081039f4bd0990e2d43
manasan-iTL <daiyaofssh2020@gmail.com>
2023-11-10 15:20:23 +0900

    fetchRestaurantViaV1TextSearch関数の追加

--- fetch.js
+++ fetch.js
@@ -34,3 +34,33 @@ export async function fetchRestaurantViaV1Nearby() {
         console.log(error)
     }
 }
+
+export async function fetchRestaurantViaV1TextSearch() {
+    const BASE_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+
+    const queryParams =  new URLSearchParams({
+        // 以下のパラメーターは必須
+        query: "市ヶ谷駅周辺のレストラン",
+        radius: 50000,
+        key: GOOGLE_PLACES_API_KEY,
+
+        // これ以降はオプション
+        type: "restaurant", // https://developers.google.com/maps/documentation/places/web-service/supported_types?hl=ja
+        language: "ja",
+        // maxprice: 0-4のいずれかの値,
+        // minprice: 0-4のいずれかの値,
+        // opennow; ,
+        // rankby: prominence/distance,
+        // pagetoken: 次の検索件数（２０件ごと）,
+        // location: ""
+    })
+
+    try {
+        const rawResponse = await fetch(`${BASE_URL}?${queryParams}`)
+        const response = await rawResponse.json()
+
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
+}
